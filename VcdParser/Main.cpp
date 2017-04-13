0484c953a407ca931d8f5a8b01291f4914d158a5
Daniel Vieira Vega <daniel.vega@acad.pucrs.br>
2017-04-13 16:35:38 -0300

    xuxu

--- VcdParser/Main.cpp
+++ VcdParser/Main.cpp
@@ -3,7 +3,10 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <map>
 #include "Parser.h"
+
+
 
 std::vector<std::string> SplitBySpace(std::string str)
 {
@@ -39,10 +42,10 @@ int main()
 
 				while (getline(vcdFile, line))
 				{
-					
 					if(line.find("$var") != std::string::npos)
 					{
 						Signal signal;
+						signal.SwitchingActivityCounter = 0;
 						std::vector<std::string> splittedLineSignal = SplitBySpace(line);
 						signal.Symbol = splittedLineSignal[3];
 						if (splittedLineSignal.size() == 6)
@@ -60,12 +63,85 @@ int main()
 						break;
 					}
 				}
-				
 
 				modules.push_back(module);
 			}
+			else if (line.find("$dumpvars") != std::string::npos)
+			{
+				std::vector<std::string> switchingActivitySignals;
+				switchingActivitySignals.push_back("0");
+				switchingActivitySignals.push_back("1");
+				switchingActivitySignals.push_back("z");
+
+				bool foundSwitchingActivity = false;
+
+				while (getline(vcdFile, line))
+				{
+					if ((line.find('0') != std::string::npos)
+						|| (line.find('1') != std::string::npos)
+						|| (line.find('z') != std::string::npos))
+					{
+						foundSwitchingActivity = false;
+
+						for (auto it = modules.begin(); it != modules.end() && !foundSwitchingActivity; ++it)
+						{
+							auto modulesIterator = *it;
+							for (auto it1 = modulesIterator.Signals.begin(); it1 != modulesIterator.Signals.end() && !foundSwitchingActivity; ++it1)
+							{
+								auto signalsIterator = *it1;
+								for (auto switchingActivityIterator = switchingActivitySignals.begin(); (switchingActivityIterator != switchingActivitySignals.end()) && !foundSwitchingActivity; ++switchingActivityIterator)
+								{
+									std::string currentSignalActivity = "";
+									if (line.find('0') != std::string::npos)
+									{
+										currentSignalActivity = '0';
+									}
+									else if (line.find('1') != std::string::npos)
+									{
+										currentSignalActivity = '1';
+									}
+									else if (line.find('z') != std::string::npos)
+									{
+										currentSignalActivity = 'z';
+									}
+
+									if (line.find(signalsIterator.Symbol) != std::string::npos)
+									{
+										if (signalsIterator.SwitchingActivityCounter == 0)
+										{
+											(*it1).SwitchingActivityCounter++;
+											(*it1).CurrentActivity = currentSignalActivity;
+											foundSwitchingActivity = true;
+										}
+										else
+										{
+											if (signalsIterator.CurrentActivity != *switchingActivityIterator)
+											{
+												(*it1).SwitchingActivityCounter++;
+												(*it1).CurrentActivity = currentSignalActivity;
+												foundSwitchingActivity = true;
+											}
+											else
+											{
+												foundSwitchingActivity = false;
+											}
+										}
+									}
+									else
+									{
+										foundSwitchingActivity = false;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 			
 		}
+
+		
+
 		vcdFile.close();
 	}
 	else {
@@ -77,7 +153,7 @@ int main()
 	for (auto it = modules.begin(); it != modules.end(); ++it)
 	{
 		std::cout << "Module: " << (*it).Name << std::endl;
-		//std::cout << "Signals count: " << (*it).Signals << std::endl;
+		std::cout << "Signals count: " << (*it).Signals.size() << std::endl;
 		std::cout << std::endl;
 	}
 
--- VcdParser/Parser.h
+++ VcdParser/Parser.h
@@ -1,15 +1,15 @@
 #pragma once
 
-struct Module
-{
-	std::string Name;
-	std::vector<Signal> Signals;
-	//unsigned Signals;
-};
-
 struct Signal
 {
 	std::string Name;
 	std::string Symbol;
+	unsigned SwitchingActivityCounter;
+	std::string CurrentActivity;
 };
 
+struct Module
+{
+	std::string Name;
+	std::vector<Signal> Signals;
+};
